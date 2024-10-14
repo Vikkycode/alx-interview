9bc129418149ae8d1026ec3f7f806f314f857e5b
Victor <vicky.robilson5528@gmail.com>
2024-10-14 04:31:01 +0100

    added lockboxes file and readme file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,26 @@
-!#/usr/bin/python
+#!/usr/bin/python
+""" canUnlockAll function """
+
+
+def canUnlockAll(boxes):
+    """
+    determines if all boxex can be unlocked
+
+    Returns:
+    bool: True if all boxes can be unlocked otherwise return False
+    """
+
+    num = len(boxes)
+
+    opened[0] = True
+    opened = [False] * num
+
+    stack = [0]
+
+    while stack:
+        cur_box = stack.pop()
+        for k in boxes[cur_box]:
+            if k < num and not opened[k]:
+                opened[k] = True
+                stack.append(k)
+    return all(opened)
