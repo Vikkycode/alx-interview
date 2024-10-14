6ccfadfdca79352afbfc9f78c92508f9f7bf89aa
Victor <vicky.robilson5528@gmail.com>
2024-10-14 05:19:17 +0100

    update lockboxes file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,23 +6,21 @@ from collections import deque
 def canUnlockAll(boxes):
     """
     determines if all boxex can be unlocked
-
-    Returns:
-    bool: True if all boxes can be unlocked otherwise return False
     """
 
     num = len(boxes)
+    
     open_boxes = set()
+    
     open_boxes.add(0)
 
     quene = deque([0])
 
     while quene:
-        curr_box = quene.popleft()
+        cur_box = quene.popleft()
 
-        for key in boxes[curr_box]:
-            if key < num and key not in open_boxes:
-                open_boxes.add(key)
-                quene.append(key)
+        keys = [k for k in boxes[cur_box] if k < num and k not in open_boxes]
+        open_boxes.update(keys)
+        quene.extend(keys)
 
     return len(open_boxes) == num
