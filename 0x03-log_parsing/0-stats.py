ba7adf3a56b4b9bed159378682d4b58509293d69
Victor <vicky.robilson5528@gmail.com>
2024-10-25 03:46:10 +0100

    update 0 stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""Module for task 0
+"""task 0
 
 Write a script that reads stdin line by line and computes metrics:
-
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>.
-(if the format is not this one, the line must be skipped).
-After every 10 lines and/or a keyboard interruption (CTRL + C), print these
-statistics from the beginning:
-    Total file size: File size: <total size>
-    where <total size> is the sum of all previous <file size> (format above)
-    Number of lines by status code:
-        possible status code: 200, 301, 400, 401, 403, 404, 405 and 500.
-        if a status code doesn’t appear or is not an integer, don’t
-        print anything for this status code.
-        format: <status code>: <number>.
-        status codes should be printed in ascending order.
 """
 
 
 import re
 
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    fp = (
+def show_input(input_line):
+    """Extracts sections of a line of an HTTP request log.
+    """
+    p = (
         r'\s*(?P<ip>\S+)\s*',
         r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
         r'\s*"(?P<request>[^"]*)"\s*',
@@ -36,7 +22,7 @@ def extract_input(input_line):
         'status_code': 0,
         'file_size': 0,
     }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(p[0], p[1], p[2], p[3], p[4])
     resp_match = re.fullmatch(log_fmt, input_line)
     if resp_match is not None:
         status_code = resp_match.group('status_code')
@@ -65,7 +51,7 @@ def update_metrics(line, total_file_size, status_codes_stats):
     Returns:
         int: The new total file size.
     '''
-    line_info = extract_input(line)
+    line_info = show_input(line)
     status_code = line_info.get('status_code', '0')
     if status_code in status_codes_stats.keys():
         status_codes_stats[status_code] += 1
